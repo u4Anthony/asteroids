9a260469f3ef8d47e9121c17777bd27b67ffadbc
Anthony <ahaddadkc@gmail.com>
2024-11-11 09:43:38 -0500

    basic player movement

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -22,6 +22,10 @@ class Player(CircleShape):
 	def rotate(self, dt):
 		self.rotation += PLAYER_TURN_SPEED * dt
 
+	def move(self, dt):
+		forward = pygame.Vector2(0, 1).rotate(self.rotation)
+		self.position += forward * PLAYER_SPEED * dt
+
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
 
@@ -29,3 +33,7 @@ class Player(CircleShape):
 			self.rotate(-dt)
 		if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
 			self.rotate(dt)
+		if keys[pygame.K_w] or keys[pygame.K_UP]:
+			self.move(dt)
+		if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+			self.move(-dt)
