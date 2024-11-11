b4c4cd080d0b9686d61a70490c7c8115d7d504b0
Anthony <ahaddadkc@gmail.com>
2024-11-10 20:36:34 -0500

    add game clock and 60 sec tick

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from constants import *
 
 def main():
 	pygame.init()
-
+	
+	clock =  pygame.time.Clock()
+	dt = 0
+	
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
@@ -11,8 +14,9 @@ def main():
 				return
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
+		tick = clock.tick(60)
+		dt = tick / 1000 # convert from milliseconds to seconds
 
-		
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
